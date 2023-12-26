5d6aeeb3e9cca93787fbd1b89596638200aead06
railway79_ <taianhnguyen12@gmail.com>
2023-12-27 04:14:46 +0700

    [Add] Post create form

---
+++ src/main/java/com/vti/BlockApp/Form/PostCreateForm.java
@@ -0,0 +1,24 @@
+package com.vti.BlockApp.Form;
+
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class PostCreateForm {
+
+
+
+
+    private String title;
+
+
+
+    private String description;
+
+
+    private String content;
+}
