5f15a0afac56d478a2939ca3633b34615346a357
railway79_ <taianhnguyen12@gmail.com>
2023-12-27 01:15:15 +0700

    [Add] Blog App

---
+++ src/main/java/com/vti/BlockApp/BlockAppApplication.java
@@ -0,0 +1,14 @@
+package com.vti.BlockApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BlockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/BlockApp/BlockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.BlockApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
