7c4e1ae497ddad77732200d37b39dea1b5495bbb
railway79_ <taianhnguyen12@gmail.com>
2023-12-27 04:19:25 +0700

    [Add] Post repository

---
+++ src/main/java/com/vti/BlockApp/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.vti.BlockApp.repository;
+
+import com.vti.BlockApp.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+
+
+
+}
